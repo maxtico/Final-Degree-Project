0b37e88519c66ea443017ed4e8a99b7838b3c339
maxtico <104825030+maxtico@users.noreply.github.com>
2022-05-12 14:08:15 +0200

    Add files via upload

---
+++ summary_nd_silent_windowed.R
@@ -0,0 +1,242 @@
+getwd()
+setwd("/home/max/Documents/3/Internship/Summary_statistics/")
+
+library(patchwork)
+library(ggplot2)
+
+###NONSYNONYMOUS
+
+##SOUTHERN
+
+#Nucleotide diversity all chromosomes
+pi.sth<-read.table("silent_southern.windowed.pi",header=T)
+hist(pi.sth$PI,br=20)
+boxplot(pi.sth$PI,ylab="diversity")
+
+#CHR1A
+pi.chr1A.S<-subset(pi.sth, CHROM=="1A")
+S1A<-ggplot(pi.chr1A.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR1A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))+geom_smooth()
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR1A.png",plot = S1A, width = 10, height=5)
+
+#CHR1B
+pi.chr1B.S<-subset(pi.sth, CHROM=="1B")
+S1B<-ggplot(pi.chr1B.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR1B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR1B.png",plot = S1B, width = 10, height=5)
+
+#CHR2A
+pi.chr2A.S<-subset(pi.sth, CHROM=="2A")
+S2A<-ggplot(pi.chr2A.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR2A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR2A.png",plot = S2A, width = 10, height=5)
+
+#CHR2B
+pi.chr2B.S<-subset(pi.sth, CHROM=="2B")
+S2B<-ggplot(pi.chr2B.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR2B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR2B.png",plot = S1B, width = 10, height=5)
+
+#CHR3A
+pi.chr3A.S<-subset(pi.sth, CHROM=="3A")
+S3A<-ggplot(pi.chr3A.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR3A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/SILENT_nuc_div_southern_CHR3A.png",plot = S3A, width = 10, height=5)
+
+#CHR3B
+pi.chr3B.S<-subset(pi.sth, CHROM=="3B")
+S3B<-ggplot(pi.chr3B.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR3B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR3B.png",plot = S3B, width = 10, height=5)
+
+#CHR4A
+pi.chr4A.S<-subset(pi.sth, CHROM=="4A")
+S4A<-ggplot(pi.chr4A.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR4A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/SILENT_nuc_div_southern_CHR4A.png",plot = S4A, width = 10, height=5)
+
+#CHR4B
+pi.chr4B.S<-subset(pi.sth, CHROM=="4B")
+S4B<-ggplot(pi.chr4B.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR4B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR4B.png",plot = S4B, width = 10, height=5)
+
+#CHR5A
+pi.chr5A.S<-subset(pi.sth, CHROM=="5A")
+S5A<-ggplot(pi.chr5A.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR5A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR5A.png",plot = S5A, width = 10, height=5)
+
+#CHR5B
+pi.chr5B.S<-subset(pi.sth, CHROM=="5B")
+S5B<-ggplot(pi.chr5B.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR5B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR5B.png",plot = S5B, width = 10, height=5)
+
+#CHR6A
+pi.chr6A.S<-subset(pi.sth, CHROM=="6A")
+S6A<-ggplot(pi.chr6A.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR6A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR6A.png",plot = S6A, width = 10, height=5)
+
+#CHR6B
+pi.chr6B.S<-subset(pi.sth, CHROM=="6B")
+S6B<-ggplot(pi.chr6B.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR6B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR6B.png",plot = S6B, width = 10, height=5)
+
+#CHR7A
+pi.chr7A.S<-subset(pi.sth, CHROM=="7A")
+S7A<-ggplot(pi.chr7A.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR7A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR7A.png",plot = S7A, width = 10, height=5)
+
+#CHR7B
+pi.chr7B.S<-subset(pi.sth, CHROM=="7B")
+S7B<-ggplot(pi.chr7B.S, aes(x=BIN_START,y=PI))+geom_point(col="red")+labs(x="position",y="diversity")+ggtitle("SOUTHERN CHR7B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_southern_CHR7B.png",plot = S7B, width = 10, height=5)
+
+##NORTHERN
+
+#Nucleotide diversity all chromosomes
+pi.nth<-read.table("silent_northern.windowed.pi",header=T)
+hist(pi.nth$PI,br=20)
+boxplot(pi.nth$PI,ylab="diversity")
+
+#CHR1A
+pi.chr1A.N<-subset(pi.nth, CHROM=="1A")
+N1A<-ggplot(pi.chr1A.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR1A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR1A.png",plot = N1A, width = 10, height=5)
+
+#CHR1B
+pi.chr1B.N<-subset(pi.nth, CHROM=="1B")
+N1B<-ggplot(pi.chr1B.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR1B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR1B.png",plot = N1B, width = 10, height=5)
+
+#CHR2A
+pi.chr2A.N<-subset(pi.nth, CHROM=="2A")
+N2A<-ggplot(pi.chr2A.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR2A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR2A.png",plot = N2A, width = 10, height=5)
+
+#CHR2B
+pi.chr2B.N<-subset(pi.nth, CHROM=="2B")
+N2B<-ggplot(pi.chr2B.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR2B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR2B.png",plot = N2B, width = 10, height=5)
+
+#CHR3A
+pi.chr3A.N<-subset(pi.nth, CHROM=="3A")
+N3A<-ggplot(pi.chr3A.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR3A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR3A.png",plot = N3A, width = 10, height=5)
+
+#CHR3B
+pi.chr3B.N<-subset(pi.nth, CHROM=="3B")
+N3B<-ggplot(pi.chr3B.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR3B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR3B.png",plot = N3B, width = 10, height=5)
+
+#CHR4A
+pi.chr4A.N<-subset(pi.nth, CHROM=="4A")
+N4A<-ggplot(pi.chr4A.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR4A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR4A.png",plot = N4A, width = 10, height=5)
+
+#CHR4B
+pi.chr4B.N<-subset(pi.nth, CHROM=="4B")
+N4B<-ggplot(pi.chr4B.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR4B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR4B.png",plot = N4B, width = 10, height=5)
+
+#CHR5A
+pi.chr5A.N<-subset(pi.nth, CHROM=="5A")
+N5A<-ggplot(pi.chr5A.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR5A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR5A.png",plot = N5A, width = 10, height=5)
+
+#CHR5B
+pi.chr5B.N<-subset(pi.nth, CHROM=="5B")
+N5B<-ggplot(pi.chr5B.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR5B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR5B.png",plot = N5B, width = 10, height=5)
+
+#CHR6A
+pi.chr6A.N<-subset(pi.nth, CHROM=="6A")
+N6A<-ggplot(pi.chr6A.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR6A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR6A.png",plot = N6A, width = 10, height=5)
+
+#CHR6B
+pi.chr6B.N<-subset(pi.nth, CHROM=="6B")
+N6B<-ggplot(pi.chr6B.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR6B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR6B.png",plot = N6B, width = 10, height=5)
+
+#CHR7A
+pi.chr7A.N<-subset(pi.nth, CHROM=="7A")
+N7A<-ggplot(pi.chr7A.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR7A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR7A.png",plot = N7A, width = 10, height=5)
+
+#CHR7B
+pi.chr7B.N<-subset(pi.nth, CHROM=="7B")
+N7B<-ggplot(pi.chr7B.N, aes(x=BIN_START,y=PI))+geom_point(col="green")+labs(x="position",y="diversity")+ggtitle("NORTHERN CHR7B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_northern_CHR7B.png",plot = N7B, width = 10, height=5)
+
+
+##DOMESTIC
+
+#Nucleotide diversity all chromosomes
+pi.dom<-read.table("silent_domestic.windowed.pi",header=T)
+hist(pi.dom$PI,br=20)
+boxplot(pi.dom$PI,ylab="diversity")
+
+#CHR1A
+pi.chr1A.D<-subset(pi.dom, CHROM=="1A")
+D1A<-ggplot(pi.chr1A.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR1A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR1A.png",plot = D1A, width = 10, height=5)
+
+#CHR1B
+pi.chr1B.D<-subset(pi.dom, CHROM=="1B")
+D1B<-ggplot(pi.chr1B.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR1B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR1B.png",plot = D1B, width = 10, height=5)
+
+#CHR2A
+pi.chr2A.D<-subset(pi.dom, CHROM=="2A")
+D2A<-ggplot(pi.chr2A.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR2A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR2A.png",plot = D2A, width = 10, height=5)
+
+#CHR2B
+pi.chr2B.D<-subset(pi.dom, CHROM=="2B")
+D2B<-ggplot(pi.chr2B.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR2B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR2B.png",plot = D2B, width = 10, height=5)
+
+#CHR3A
+pi.chr3A.D<-subset(pi.dom, CHROM=="3A")
+D3A<-ggplot(pi.chr3A.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR3A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR3A.png",plot = D3A, width = 10, height=5)
+
+#CHR3B
+pi.chr3B.D<-subset(pi.dom, CHROM=="3B")
+D3B<-ggplot(pi.chr3B.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR3B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR3B.png",plot = D3B, width = 10, height=5)
+
+#CHR4A
+pi.chr4A.D<-subset(pi.dom, CHROM=="4A")
+D4A<-ggplot(pi.chr4A.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR4A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR4A.png",plot = D4A, width = 10, height=5)
+
+#CHR4B
+pi.chr4B.D<-subset(pi.dom, CHROM=="4B")
+D4B<-ggplot(pi.chr4B.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR4B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR4B.png",plot = D4B, width = 10, height=5)
+
+#CHR5A
+pi.chr5A.D<-subset(pi.dom, CHROM=="5A")
+D5A<-ggplot(pi.chr5A.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR5A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR5A.png",plot = D5A, width = 10, height=5)
+
+#CHR5B
+pi.chr5B.D<-subset(pi.dom, CHROM=="5B")
+D5B<-ggplot(pi.chr5B.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR5B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR5B.png",plot = D5B, width = 10, height=5)
+
+#CHR6A
+pi.chr6A.D<-subset(pi.dom, CHROM=="6A")
+D6A<-ggplot(pi.chr6A.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR6A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR6A.png",plot = D6A, width = 10, height=5)
+
+#CHR6B
+pi.chr6B.D<-subset(pi.dom, CHROM=="6B")
+D6B<-ggplot(pi.chr6B.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR6B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR6B.png",plot = D6B, width = 10, height=5)
+
+#CHR7A
+pi.chr7A.D<-subset(pi.dom, CHROM=="7A")
+D7A<-ggplot(pi.chr7A.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR7A")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR7A.png",plot = D7A, width = 10, height=5)
+
+#CHR7B
+pi.chr7B.D<-subset(pi.dom, CHROM=="7B")
+D7B<-ggplot(pi.chr7B.D, aes(x=BIN_START,y=PI))+geom_point()+labs(x="position",y="diversity")+ggtitle("DOMESTIC CHR7B")+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0.0,0.005))
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/SILENT_ND_WINDOWED_PLOTS/silent_nuc_div_domestic_CHR7B.png",plot = D7B, width = 10, height=5)
+
+summ<-(W1A | S1A | N1A) / (W1B | S1B | N1B) / (W2A | S2A | N2A) / (W2B | S2B | N2B) / (W3A | S3A | N3A) / (W3B | S3B | N3B) / (W4A | S4A | N4A) / (W4B | S4B | N4B) / (W5A | S5A | N5A) / (W5B | S5B | N5B) / (W6A | S6A | N6A) / (W6B | S6B | N6B) / (W7A | S7A | N7A) / (W7B | S7B | N7B)
+ggsave("/home/max/Documents/3/Internship/Summary_statistics/gen_diversity_by_chr_windowed_synonymous_variants.png",plot=summ,width = 20, height=30)
